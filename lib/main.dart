9b5fb424929268155e12c2a75b2c9a51515741ef
= <=>
2023-08-08 11:16:26 -0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ToDoList());
+}
+
+class ToDoList extends StatelessWidget {
+  const ToDoList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
